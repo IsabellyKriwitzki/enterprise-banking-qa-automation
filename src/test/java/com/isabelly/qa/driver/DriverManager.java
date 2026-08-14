7d68f56d0a315e802462c67f52c680bca679a339
Isabelly Kriwitzki <isabeellykriwitzki2@gmail.com>
2026-08-14 18:56:50 +0200

    Fix Chrome execution in CI environment

--- src/test/java/com/isabelly/qa/driver/DriverManager.java
+++ src/test/java/com/isabelly/qa/driver/DriverManager.java
@@ -2,6 +2,7 @@ package com.isabelly.qa.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
@@ -14,7 +15,15 @@ public class DriverManager {
     public static void initializeDriver() {
 
         if (driver == null) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
     }
